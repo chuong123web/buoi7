78428794c70142837754138ccf0e967cce121051
chuong123web <nguyenpnchuong@students.hueic.edu.vn>
2025-10-15 19:05:42 +0700

    Buổi 7: Bài tập JS

--- bai3.js
+++ bai3.js
@@ -15,15 +15,3 @@ const timSinhVienTheoSDT = (ds, sdt) =>
 
 const locSinhVienTheoTuoi = (ds, tuoi) =>
     ds.filter(sv => sv.tuoi === tuoi);
-dsSinhVien.push(nhapSinhVien("Nguyễn Văn A", 20, "Hà Nội", "0123456789"));
-dsSinhVien.push(nhapSinhVien("Trần Thị B", 21, "Đà Nẵng", "0987654321"));
-dsSinhVien.push(nhapSinhVien("Lê Văn C", 20, "Hải Phòng", "0112233445"));
-
-console.log("Danh sách sinh viên:");
-inDanhSachSV(dsSinhVien);
-
-console.log("Danh sách sau khi sắp xếp:");
-inDanhSachSV(xapXepDanhSachSV(dsSinhVien));
-
-console.log("Tìm sinh viên theo SĐT 0987654321:", timSinhVienTheoSDT(dsSinhVien, "0987654321"));
-console.log("Lọc sinh viên theo tuổi = 20:", locSinhVienTheoTuoi(dsSinhVien, 20));
